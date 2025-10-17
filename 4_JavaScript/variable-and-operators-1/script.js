01552cefdcb89e649a981626a7a8abbb1839b6c9
avarshvir <avarshvir@gmail.com>
2025-10-17 14:12:36 +0530

    JavaScript Variable and Operators

---
+++ 4_JavaScript/variable-and-operators-1/script.js
@@ -0,0 +1,17 @@
+let arsh_name = "Arshvir";
+let profession = "AI Software Engineer";
+
+console.log(arsh_name);
+console.log(profession);
+
+const pi = 3.14;
+
+
+console.log("value of pi is as const: "+ pi);
+console.log("const is for constant value and we can't modify it.")
+console.log("while let keyword help us to modify our value :)")
+
+var org = "Jaiho Digital"
+console.log(org)
+
+console.log("Number: ", (3 + 2) - 76 * (1 + 1))
