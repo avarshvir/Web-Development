669e1d8c02d63f130da4df247ae79e7fe36d4da5
avarshvir <avarshvir@gmail.com>
2025-10-17 15:25:16 +0530

    intro to JS

---
+++ 4_JavaScript/variable-and-operators-1/assignments/script.js
@@ -0,0 +1,29 @@
+console.log(23+97);
+
+console.log(1+2+3+4+5+6);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log("a: "+a);
+
+a = 5;
+console.log("a: ",a);
+
+let b = 7 * a
+console.log("b:",b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual/max;
+console.log(percentage);
+
+// note: no "use strict" in this example
+
+num = 5; // the variable "num" is created if it didn't exist
+console.log(num);
+alert(num); // 5
+
+// "use strict";  -> always on top if use
+//num2 = 5; // error: num is not defined
+//console.log(num2)
