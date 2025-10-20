abb6f79ab770a0a09549362f84b82fd10b6bfc80
avarshvir <avarshvir@gmail.com>
2025-10-20 16:07:52 +0530

    built in functions

---
+++ 4_JavaScript/functions-4/built-in-fun.js
@@ -0,0 +1,19 @@
+const myText = "I am a string";
+const newString = myText.replace("string", "sausage");
+console.log(newString);
+
+console.log("----------------------------------------");
+
+const myArray = ["I", "love", "chocolate", "frogs"];
+const madeAString = myArray.join(" ");
+console.log(madeAString);
+const madeAnotherString = myArray.join();
+console.log(madeAnotherString);
+
+console.log("----------------------------------------");
+
+const myNumber = Math.random();
+console.log(myNumber);
+
+console.log("----------------------------------------");
+
