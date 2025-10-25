3b32edfd3be8454f2da6a3a9af97acf820a74ff9
avarshvir <avarshvir@gmail.com>
2025-10-25 21:38:46 +0530

    fizz buzz problem

---
+++ 0_Basics/problem-solving/fizz-buzz-problem.js
@@ -0,0 +1,16 @@
+const user_input = parseInt(prompt("Enter the End number:"));
+
+for(let i = 1; i <= user_input; i++){
+    if(i%3==0 && i%5==0){
+        console.log("FizzBuzz");
+    }
+    else if(i%3==0){
+        console.log("Fizz");
+    }
+    else if(i%5==0){
+        console.log("Buzz");
+    }
+    else{
+        console.log(i);
+    }
+}
