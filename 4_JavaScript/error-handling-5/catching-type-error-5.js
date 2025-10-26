04a66b09217cbdd0f7eb34420a889b4f71c74af9
avarshvir <avarshvir@gmail.com>
2025-10-26 15:03:11 +0530

    catching a type error

---
+++ 4_JavaScript/error-handling-5/catching-type-error-5.js
@@ -0,0 +1,8 @@
+try {
+  null.f();
+} catch (e) {
+  console.log(e instanceof TypeError); // true
+  console.log(e.message); // "null has no properties"
+  console.log(e.name); // "TypeError"
+  console.log(e.stack); // Stack of the error
+}
